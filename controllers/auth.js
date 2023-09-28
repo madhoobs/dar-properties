243d94f964fa0f36febb78fa92868d57faa2c467
Hasan Almadhoob <hasan@madhoob.com>
2023-09-28 23:15:49 +0300

    Signup bug fixes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,52 +1,54 @@
+const User = require('../models/User')
 
-const User = require("../models/User")
+const bcrypt = require('bcryptjs')
 
-const bcrypt = require("bcryptjs")
-
-let passport = require("../helper/ppConfig")
+let passport = require('../helper/ppConfig')
 
 const salt = 8
 
-exports.auth_signup_get=(req,res)=>{
-res.render("auth/signup")
+exports.auth_signup_get = (req, res) => {
+  res.render('auth/signup')
 }
 
-exports.auth_signup_post =(req,res)=>{
-    if (req.body.inputPassword === req.body.inputPassword2){
+exports.auth_signup_post = (req, res) => {
+  console.log(req.body)
+  if (req.body.password === req.body.confirmPassword) {
     let user = User(req.body)
 
-    let hash =bcrypt.hashSync(req.body.inputPassword, salt)
-    user.inputPassword = hash 
-    console.log(hash)
+    // if (req.body.isCompany) {
+    //   user
+    // }
+    let hash = bcrypt.hashSync(req.body.password, salt)
+    user.password = hash
 
-    user.save()
-    .then(() => {
-        res.redirect("/")
-    })
-    .catch((err) => {
+    user
+      .save()
+      .then(() => {
+        res.redirect('/')
+      })
+      .catch((err) => {
         console.log(err)
-    })
-    } 
-else {
-    res.redirect("/auth/signup")
-}
-}
-    
-    
-
-exports.auth_login_get = (req , res) => {
-    res.render("auth/login")
+      })
+  } else {
+    res.redirect('/auth/signup')
+    // Let's add a clear error message here
+  }
 }
 
-exports.auth_login_post = 
-passport.authenticate("local" , {
-    successRedirect : "/" ,
-    failureRedirect : "/auth/login" 
+exports.auth_login_get = (req, res) => {
+  res.render('auth/login')
+}
+
+exports.auth_login_post = passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth/login'
 })
 
-exports.auth_logout_get = (req , res) => {
-    req.logout(function(err) {
-        if(err) { return next(err)}
-        res.redirect("/auth/login")
-    })
+exports.auth_logout_get = (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err)
+    }
+    res.redirect('/auth/login')
+  })
 }
