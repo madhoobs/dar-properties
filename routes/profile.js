345f3c7dfe64804555940cdd323bb9eb73cbf3d3
Zainab <zainabsameer197@gmail.com>
2023-10-01 10:35:40 +0300

    profile

---
+++ routes/profile.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const multer  = require('multer')
+// const upload = multer({ dest: 'uploads/' })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/public/uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
+const app = express()
+
+app.get('/profile', upload.single('avatar'), )
+
+app.post('/profile', upload.single('avatar'), )
+
