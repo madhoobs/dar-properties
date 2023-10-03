a7e23650c94acde54ae0cfd051f2e49bbe3e1e26
Zainab <zainabsameer197@gmail.com>
2023-10-03 15:02:57 +0300

    change profile pic

--- routes/profile.js
+++ routes/profile.js
@@ -6,13 +6,14 @@ router.use(express.urlencoded({ extended: true }))
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/public/uploads')
+    cb(null, './public/uploads')
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '' + Date.now() + '' + file.originalname)
   }
 })
-var upload = multer({ storage: storage }).single('uploads')
+var upload = multer({ storage: storage }).single('profileImage')
+
 
 const userCtrl = require('../controllers/auth')
 const isLoggedln = require('../helper/isLoggedln')
@@ -22,5 +23,7 @@ router.get('/profile/edit', isLoggedln, userCtrl.profile_edit_get)
 router.post('/profile/edit', userCtrl.profile_edit_post)
 router.get("/profile/changepassword" , userCtrl.profile_changePassword_get)
 router.post("/profile/changepassword" , userCtrl.profile_changePassword_post)
+router.post("/profile/editProfileImage" , upload, userCtrl.profile_changepic_get)
+
 
 module.exports = router
