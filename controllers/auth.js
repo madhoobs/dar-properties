f72bc8136abad43ba995e0c6c63ce4e4aebbb1a3
Hasan Almadhoob <hasan@madhoob.com>
2023-10-04 11:36:17 +0300

    Fixing signup upload

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,8 +30,8 @@ exports.auth_signup_post = (req, res) => {
 
     let hash = bcrypt.hashSync(req.body.password, salt)
     user.password = hash
-    if (req.file.filename){ 
-    user.profileImage = req.file.filename 
+    if (req.body.profileImage.filename) {
+      user.profileImage = req.body.profileImage.filename
     }
     user
       .save()
@@ -41,7 +41,7 @@ exports.auth_signup_post = (req, res) => {
       .catch((err) => {
         res.send('Try Again')
         console.log(err)
-      }) 
+      })
   } else {
     res.redirect('/auth/signup')
     // Let's add a clear error message here
@@ -67,13 +67,13 @@ exports.auth_logout_get = (req, res) => {
 }
 
 exports.profile_show_get = (req, res) => {
-      User.findById(req.query.id)
-        .then((user) => {
-          res.render('profile/profile', { user })
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+  User.findById(req.query.id)
+    .then((user) => {
+      res.render('profile/profile', { user })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 //Get Edit profile Page
@@ -90,75 +90,77 @@ exports.profile_edit_get = (req, res) => {
 
 exports.profile_edit_post = async (req, res) => {
   const userId = req.body.id
-  const { firstName , lastName,companyName,companyCR,city,address,phone,password} =
-    req.body
+  const {
+    firstName,
+    lastName,
+    companyName,
+    companyCR,
+    city,
+    address,
+    phone,
+    password
+  } = req.body
 
-    try {
-      let updatedUser = {
-        firstName,
-        lastName,
-        companyName,
-        companyCR,
-        city,
-        address,
-        phone,
-   
-      }
-      if (password) {
-        const salt = await bcrypt.genSalt(15)
-        const hashedPassword = await bcrypt.hash(password, salt)
-        updatedUser.password = hashedPassword
-      }
-      console.log(userId)
-      console.log(updatedUser)
-      await User.findByIdAndUpdate(userId, updatedUser)
-      res.redirect("/profile?id="+userId)
-    } catch (err) {
-      console.log(err)
-      res.send("Error updating user.")
+  try {
+    let updatedUser = {
+      firstName,
+      lastName,
+      companyName,
+      companyCR,
+      city,
+      address,
+      phone
     }
+    if (password) {
+      const salt = await bcrypt.genSalt(15)
+      const hashedPassword = await bcrypt.hash(password, salt)
+      updatedUser.password = hashedPassword
+    }
+    console.log(userId)
+    console.log(updatedUser)
+    await User.findByIdAndUpdate(userId, updatedUser)
+    res.redirect('/profile?id=' + userId)
+  } catch (err) {
+    console.log(err)
+    res.send('Error updating user.')
+  }
 }
 //change password
-exports.profile_changePassword_get=(req,res)=>{
+exports.profile_changePassword_get = (req, res) => {
   res.render('profile/changepassword')
 }
-exports.profile_changePassword_post=(req,res)=>{
-  
-    User.findById(req.body.user).then((user)=>{
-      const equals=bcrypt.compareSync(req.body.password, user.password)
-        if(!equals){
-          res.redirect('/profile/changepassword')
-        }
-        else if (req.body.newPassword === req.body.confirmPassword) {
-          let hash = bcrypt.hashSync(req.body.newPassword, salt)
-          user.password=hash
-          user.save().then(()=>{
-            res.redirect('/auth/login')
-          })
-        }
-        else{
-          res.redirect('/profile/changepassword')
-        }
-      
-      
-    }).catch(err=>{console.log(err)})
-  
+exports.profile_changePassword_post = (req, res) => {
+  User.findById(req.body.user)
+    .then((user) => {
+      const equals = bcrypt.compareSync(req.body.password, user.password)
+      if (!equals) {
+        res.redirect('/profile/changepassword')
+      } else if (req.body.newPassword === req.body.confirmPassword) {
+        let hash = bcrypt.hashSync(req.body.newPassword, salt)
+        user.password = hash
+        user.save().then(() => {
+          res.redirect('/auth/login')
+        })
+      } else {
+        res.redirect('/profile/changepassword')
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 //change profile pic
 
 exports.profile_changepic_get = (req, res) => {
-
-  console.log("1111")
-
-  if (req.file.filename){ 
-    userProfileImage = req.file.filename 
-    }
-console.log("id", req.body.id)
-    User.findByIdAndUpdate(req.body.id ,{profileImage: userProfileImage})
+  if (req.file.filename) {
+    userProfileImage = req.file.filename
+  }
+  console.log('id', req.body.id)
+  User.findByIdAndUpdate(req.body.id, { profileImage: userProfileImage })
     .then(() => {
       res.redirect(`/profile?id=${req.body.id}`)
-    }) 
+    })
     .catch((err) => {
       res.send(err)
     })
